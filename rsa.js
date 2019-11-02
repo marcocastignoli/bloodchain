1e4304ee4f1c82a8bec5b7e565e5c1d192c62508
marcocastignoli <marco.castignoli@gmail.com>
2019-11-02 18:59:09 +0100

    fix rsa generation

--- rsa.js
+++ rsa.js
@@ -1,7 +1,6 @@
-
 const fs = require('fs');
 const NodeRSA = require('node-rsa');
 const key_test = new NodeRSA({b: 512})
 
-fs.writeFile('public_key', key_test.exportKey('pkcs8-public'))
-fs.writeFile('private_key', key_test.exportKey('pkcs8'))
+fs.writeFile('public_key', key_test.exportKey('pkcs8-public'), () => {})
+fs.writeFile('private_key', key_test.exportKey('pkcs8'), () => {})
