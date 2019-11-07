3a28ba06ba43e56ab8074642362a2a6dc833cbca
marcocastignoli <marco.castignoli@gmail.com>
2019-11-07 16:14:04 +0100

    remove logging and silent ipfs

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const BloodyNode = require('./BloodyNode.js')
-const NodeRSA = require('node-rsa');
 const IPFS = require('ipfs')
 const crypto = require('libp2p-crypto')
 const PeerId = require('peer-id')
@@ -36,7 +35,7 @@ async function init(app) {
 
   process.stdout.write("Connecting to IPFS... ");
   const ipfs = await IPFS.create({
-    silent: false,
+    silent: true,
     init: {
       privateKey: peerJSON.privKey,
     },
@@ -50,58 +49,10 @@ async function init(app) {
         ],
       },
     }
-    /* relay: {
-      enabled: true
-    },
-    config: {
-      "Addresses": {
-        "Swarm": [
-          '/ip4/51.75.30.53/tcp/4004/ws/ipfs/QmUeLcCfeR33xGcR8DBta2gG8EcfZ9UygLxLdaXMYQMzFA'
-        ],
-        "API": "",
-        "Gateway": ""
-      },
-      "Discovery": {
-        "MDNS": {
-          "Enabled": false,
-          "Interval": 10
-        },
-        "webRTCStar": {
-          "Enabled": true
-        }
-      }
-    } */
   })
   process.stdout.write("OK\n");
 
-
-  const peerId = (await ipfs.id()).id
-  // TODO: now I'm generating the basic blockchain every time
-
-
-  process.stdout.write("Making you rich... ");
-
-  /*   let blocks = []
-    let genesisBlock = {
-      transactions: [
-        { fromAddress: '1h3h12ui3h1i2u3h1iu2hi12ui3h1ui23hi1u2h3iu1h23333', toAddress: peerId, amount: 1000, fee: 0 },
-        { fromAddress: peerId, toAddress: '1h3h12ui3h1i2u3h1iu2hi12ui3h1ui23hi1u2h3iu1h23333', amount: 900, fee: 0 },
-      ]
-    }
-    let hash = ''
-    const difficulty = 3
-    let nonce = 0
-    while (hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
-      nonce++
-      hash = sha256(nonce + JSON.stringify(genesisBlock)).toString()
-    }
-    genesisBlock.hash = hash
-    genesisBlock.nonce = nonce
-    blocks.push(genesisBlock) */
   let blocks = JSON.parse(fs.readFileSync('./blockchain'))
-  process.stdout.write("OK\n");
-
-  //const blocks = [{"transactions":[{"fromAddress":"1h3h12ui3h1i2u3h1iu2hi12ui3h1ui23hi1u2h3iu1h23333","toAddress":"QmVe3zDyfmNRUDMR3eg9BQJk88RhosxKgwKWDSxH67Tjdd","amount":1000,"fee":0},{"fromAddress":"QmVe3zDyfmNRUDMR3eg9BQJk88RhosxKgwKWDSxH67Tjdd","toAddress":"1h3h12ui3h1i2u3h1iu2hi12ui3h1ui23hi1u2h3iu1h23333","amount":900,"fee":0}],"hash":{"type":"Buffer","data":[0,0,1,110,54,223,196,71,60,148,207,97,72,144,42,18]}}]
 
   process.stdout.write("Initializing the node... ");
   const node = await BloodyNode.create({
@@ -140,7 +91,14 @@ async function init(app) {
     return res.send(JSON.stringify(balance))
   })
   app.listen(process.env.HTTP_PORT)
-  process.stdout.write(`\nWallet API available at http://localhost:${process.env.HTTP_PORT}\n\nYour public hash is: ${(await ipfs.id()).id}\n`);
+  process.stdout.write(`
+Wallet API available at http://localhost:${process.env.HTTP_PORT}
+    
+Your public hash is: ${(await ipfs.id()).id}
+
+These are your addresses:
+`);
+  ipfs._peerInfo.multiaddrs.forEach(element => console.log(element.toString()))
 }
 
 console.log(`
